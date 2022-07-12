80ab1afc909ce81fd4b92fb0d9cf32d03c83712d
Alan <baranalaan@gmail.com>
2022-07-12 13:04:02 +0200

    add OrderRepo

---
+++ src/main/java/am/alanmiste/ordersystemspring/shop/order/OrderRepo.java
@@ -0,0 +1,23 @@
+package am.alanmiste.ordersystemspring.shop.order;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OrderRepo {
+
+    private final Map<Integer, Order> orders = new HashMap<>();
+
+    public void addOrder(Order order) {
+        orders.put(order.id(), order);
+    }
+
+    public Order getOrder(int orderId) {
+        return orders.get(orderId);
+    }
+
+    public List<Order> listOrders() {
+        return new ArrayList<>(orders.values());
+    }
+}
