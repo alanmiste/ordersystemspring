403847f9c665e71a60c3cc5d5227992652df722e
Alan <baranalaan@gmail.com>
2022-07-12 12:46:54 +0200

    initial commit

---
+++ src/main/java/am/alanmiste/ordersystemspring/OrdersystemspringApplication.java
@@ -0,0 +1,13 @@
+package am.alanmiste.ordersystemspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersystemspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersystemspringApplication.class, args);
+    }
+
+}
