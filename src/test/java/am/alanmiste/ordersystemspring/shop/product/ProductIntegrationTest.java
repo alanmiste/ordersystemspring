968210db82fbde579309e60e80b7f0cc2b2814dc
Alan <baranalaan@gmail.com>
2022-07-13 21:45:46 +0200

    update ProductIntegrationTest

--- src/test/java/am/alanmiste/ordersystemspring/shop/product/ProductIntegrationTest.java
+++ src/test/java/am/alanmiste/ordersystemspring/shop/product/ProductIntegrationTest.java
@@ -6,6 +6,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ProductIntegrationTest {
@@ -14,7 +18,22 @@ public class ProductIntegrationTest {
     MockMvc mockMvc;
 
     @Test
-    void getOneProduct(){
+    void getOneProduct() throws Exception {
+        //Given
+        mockMvc
+                //When
+                .perform(
+                        get("/api/products/3")
+                )
+
+                //Then
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                            "id": 3,
+                            "name": "Zitrone"
+                        }
+                        """));
 
     }
 }
